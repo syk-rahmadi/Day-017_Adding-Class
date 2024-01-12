12fe7f5e272f6e6472c24e575de2fec23d4290ff
syk-rahmadi <98292604+syk-rahmadi@users.noreply.github.com>
2024-01-12 12:44:44 +0800

    Day 17 part of 100 days python learning

---
+++ explain.py
@@ -0,0 +1,9 @@
+# Constructor
+
+class Car:
+    def __init__(self, seats):
+        self.seats = seats
+        #iniatialise attributes
+
+my_car = Car(5)
+
---
+++ question_model.py
@@ -0,0 +1,7 @@
+class Question :
+    def __init__(self, QuestionText, QuestionAnswer):
+        self.defaultQuestion = QuestionText
+        self.answer = QuestionAnswer
+
+#new_question = Question ("stfggg", "True")
+#print(new_question.defaultQuestion)
